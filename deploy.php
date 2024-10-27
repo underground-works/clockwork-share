b3023eabdc0c8e9303394b500156cc9afcb26f18
its <its@luzer.sk>
2024-10-28 00:38:10 +0100

    Updated to Laravel 9 and Deployer 7.

--- deploy.php
+++ deploy.php
@@ -1,8 +1,6 @@
 <?php namespace Deployer;
 
-require __DIR__ . '/vendor/autoload.php';
-
-require 'recipe/composer.php';
+require 'recipe/laravel.php';
 
 // Project name
 set('application', 'Clockwork Share');
@@ -10,14 +8,7 @@ set('application', 'Clockwork Share');
 // Project repository
 set('repository', 'https://github.com/underground-works/clockwork-share.git');
 
-// Shared files/dirs between deploys
-add('shared_files', [
-	'.env'
-]);
-
-add('shared_dirs', [
-	'storage'
-]);
+set('writable_dirs', []);
 
 set('default_stage', 'production');
 
@@ -30,36 +21,6 @@ set('user', function () {
 // Hosts
 
 host('arizona')
-	->stage('production')
-	->user('its')
+	->set('labels', [ 'stage' => 'production' ])
+	->set('user', 'its')
 	->set('deploy_path', '/Sites/its/clockwork.underground.works');
-
-// Tasks
-
-desc('Execute artisan config:cache');
-task('artisan:config:cache', function () {
-	run('{{bin/php}} {{release_path}}/artisan config:cache');
-});
-
-desc('Execute artisan route:cache');
-task('artisan:route:cache', function () {
-	run('{{bin/php}} {{release_path}}/artisan route:cache');
-});
-
-desc('Execute artisan storage:link');
-task('artisan:storage:link', function () {
-	run('{{bin/php}} {{release_path}}/artisan storage:link');
-});
-
-desc('Prepare a new release');
-task('prepare', [
-	'artisan:config:cache',
-	'artisan:route:cache',
-	'artisan:storage:link'
-]);
-
-// Prepare app before symlinking new release.
-before('deploy:symlink', 'prepare');
-
-// [Optional] if deploy fails automatically unlock.
-after('deploy:failed', 'deploy:unlock');
